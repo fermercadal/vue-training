5aef172668503bc9f7c7e4e5a1fe7725883b48e9
Fernando Mercadal <fermercadal@outlook.com>
2021-01-02 18:28:33 -0300

    Update starter project

--- 101-project/app.js
+++ 101-project/app.js
@@ -1,30 +1,9 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
-      goals: [],
-      enteredValue: ''
-    };
-  },
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = '';
+      courseGoal: 'Finish the course and learn Vue'
     }
   }
-}).mount('#app');
+});
 
-/*
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
-
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
-
-buttonEl.addEventListener('click', addGoal);
-*/
+app.mount('#user-goal');
