4a1115c1217aeb7985db61beebca6edc643e4117
Fernando Mercadal <fermercadal@outlook.com>
2021-01-03 17:42:12 -0300

    Listen to an input event

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: 'NN'
     };
   },
   methods: {
-    add() {
-      this.counter++;
+    setName(event) {
+      this.name = event.target.value;
     },
-    reduce() {
-      this.counter--;
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
     }
   }
 });
